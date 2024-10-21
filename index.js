86298ab0af2fe448d66f24e1e2627746d793d722
Justin Fanton <justin.fanton@gmail.com>
2024-10-21 14:56:33 +0200

    Lab completed

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let maxValue = this.items[0];
+    for (let i = 1; i < this.items.length; i++) {
+      if (this.items[i] > maxValue) {
+        maxValue = this.items[i];
+      }
+    }
+    return maxValue;
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let minValue = this.items[0];
+    for (let i = 1; i < this.items.length; i++) {
+      if (this.items[i] < minValue) {
+        minValue = this.items[i];
+      }
+    }
+    return minValue;
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    const arrSum = this.items.reduce((a, b) => a + b, 0);
+    return arrSum;
+  } 
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let arrAverage = 0;
+    for(let i = 0; i < this.items.length; i++){
+      arrAverage += this.items[i];
+    }
+    return arrAverage / this.items.length;
+  }
 }
 
 module.exports = SortedList;
